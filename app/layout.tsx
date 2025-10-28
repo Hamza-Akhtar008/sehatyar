e8cfe7dc1aef3315e4d923ad5e593bd4678f7568
Zainab Iqbal <124600284+zainabkhan9118@users.noreply.github.com>
2025-10-28 12:25:57 +0500

    Layout fix and merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 "use client"
 
 import { AuthProvider } from "@/src/contexts/AuthContext"
@@ -6,12 +5,6 @@ import { Toaster } from "react-hot-toast"
 import "./globals.css"
 import { Geist, Geist_Mono, Montserrat, Plus_Jakarta_Sans, Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
-=======
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat, Plus_Jakarta_Sans, Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
->>>>>>> Stashed changes
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,52 +28,16 @@ const plusJakarta = Plus_Jakarta_Sans({
   weight: ["500", "800"],
   display: "swap",
 });
-<<<<<<< Updated upstream
-=======
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
-
-export const metadata: Metadata = {
-  title: "Sehat Yar",
-  description: "A medical plateform",
-};
->>>>>>> Stashed changes
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-  weight: ["400"],
-  display: "swap",
-});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-<<<<<<< Updated upstream
-    <html lang="en" suppressHydrationWarning className={`${montserrat.variable} ${plusJakarta.variable} ${inter.variable} ${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning className={`${montserrat.variable} ${plusJakarta.variable} ${geistSans.variable} ${geistMono.variable}`}>
       <body suppressHydrationWarning>
         <AuthProvider>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
             {children}
             <Toaster position="top-right" />
-=======
-    <>
-  <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} ${montserrat.variable} ${plusJakarta.variable} ${inter.variable}`}>
-        <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-            <main>{children}</main>
->>>>>>> Stashed changes
           </ThemeProvider>
         </AuthProvider>
       </body>
