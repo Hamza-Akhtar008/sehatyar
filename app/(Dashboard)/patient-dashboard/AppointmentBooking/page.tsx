1c00aa8cdfec455d92454a4cd04016b1e1bfbe68
fahad farman <fahadfarman00@gmail.com>
2025-10-28 19:11:46 +0500

    working on flow of appointment booking in pateint dashboard

---
+++ app/(Dashboard)/patient-dashboard/AppointmentBooking/page.tsx
@@ -0,0 +1,13 @@
+'use client '
+import AppointmentBooking from '@/components/landing/appointment-booking'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <AppointmentBooking />;
+    </div>
+  )
+}
+
+export default page
---
+++ app/(Dashboard)/patient-dashboard/doctor-Profile/page.tsx
@@ -0,0 +1,12 @@
+import { DoctorProfile } from '@/components/landing'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <DoctorProfile />
+    </div>
+  )
+}
+
+export default page
